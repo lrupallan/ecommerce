8849b0ae11a76c7997cd6566f3c1aef15720faef
LuRupallan <lu.rupallan@gmail.com>
2022-10-11 17:05:21 -0300

    Cambios Item Detail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -25,7 +25,8 @@ const ItemDetail = ({item}) => {
       <p className={estilos.itemStock}>Stock: {item.stock}</p>
       </div>
       <div className={estilos.containerSecundario}>
-      <p className={estilos.itemDescription}>Melkor fue encontrado por Paula detrás de una estacion de servicio muy deteriorado y con signos de que había sido atropellado. Al llevarlo a la veterinaria y ver que tenia una pata fracturada, encontraron también que estaba deshidratado y mal nutrido. <br/> El veterinario estima que debe tener entre 9 y 10 años, y que por su comportamiento, alguna vez tuvo dueños. <br/> Paula lo recibió en su casa como hogar transitorio hasta encontrar una familia que lo adopte, pero necesita colaboración para saldar la deuda que quedó en la veterinaria por la atención y las vacunas que le suministraron, tambien para proveerle un alimento de calidad que ayude a su recuperación. <br/><br/> Con tu donacion de UN módulo ya estás ayudando a Paula para que pueda seguir asistiendo a otros animalitos que lo necesiten como Melkor.</p>
+      <p className={estilos.itemDescription}>{item.description}</p>
+      <p className={estilos.itemDescription}><em> <b>Con tu donación de UN módulo ya estás ayudando a que muchos animales que lo necesitan, como {item.title}, tengan una vida mejor.</b></em></p>
       {cantidad === 0 ? (
         <Counter 
         stock={item.stock} 
